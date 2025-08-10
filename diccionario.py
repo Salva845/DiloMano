65858f4f80cd4089f91be3ef8d31d5d6c2eb3f59
angel <angel@LAPTOP-VDDMG1SK.local>
2025-08-10 15:58:37 -0600

    Se eliminó diccionario

--- diccionario.py
+++
@@ -1,8 +0,0 @@
-# diccionario.py
-diccionario_señas = {
-    "hola": "señas/hola.mp4",
-    "gracias": "señas/gracias.mp4",
-    "como": "señas/como.mp4",
-    "estas": "señas/estas.mp4",
-    "bien": "señas/bien.mp4"
-}
