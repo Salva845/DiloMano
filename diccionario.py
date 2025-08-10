663d4d2d913d8b19773a9e6fd70da580b6547b5e
angel <angel@LAPTOP-VDDMG1SK.local>
2025-08-10 16:33:41 -0600

    saludo4

--- diccionario.py
+++ diccionario.py
@@ -1,13 +1,1 @@
-<<<<<<< HEAD
-hola de nuevo
-=======
-# diccionario.py
-diccionario_seÃƒÂ±as = {
-    "hola": "seÃƒÂ±as/hola.mp4",
-    "gracias": "seÃƒÂ±as/gracias.mp4",
-    "como": "seÃƒÂ±as/como.mp4",
-    "estas": "seÃƒÂ±as/estas.mp4",
-    "bien": "seÃƒÂ±as/bien.mp4"
-}
-hola angel
->>>>>>> master
+hola
