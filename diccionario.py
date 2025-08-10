77f76edce8be2f2bcdc9d5912ee0bf70eb18ff66
angel <angel@LAPTOP-VDDMG1SK.local>
2025-08-10 16:10:01 -0600

    Nuevo

---
+++ diccionario.py
@@ -0,0 +1,8 @@
+# diccionario.py
+diccionario_seÃƒÂ±as = {
+    "hola": "seÃƒÂ±as/hola.mp4",
+    "gracias": "seÃƒÂ±as/gracias.mp4",
+    "como": "seÃƒÂ±as/como.mp4",
+    "estas": "seÃƒÂ±as/estas.mp4",
+    "bien": "seÃƒÂ±as/bien.mp4"
+}
